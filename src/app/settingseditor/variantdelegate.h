c97bbbefe8ec029394fa354b9119cae59c7f6828
Etienne Tourigny <etourigny.dev@gmail.com>
2013-01-06 19:26:14 -0200

    indentation update

--- src/app/settingseditor/variantdelegate.h
+++ src/app/settingseditor/variantdelegate.h
@@ -38,46 +38,46 @@
  **
  ****************************************************************************/
 
- #ifndef VARIANTDELEGATE_H
- #define VARIANTDELEGATE_H
+#ifndef VARIANTDELEGATE_H
+#define VARIANTDELEGATE_H
 
- #include <QItemDelegate>
- #include <QRegExp>
+#include <QItemDelegate>
+#include <QRegExp>
 
- class VariantDelegate : public QItemDelegate
- {
-     Q_OBJECT
+class VariantDelegate : public QItemDelegate
+{
+    Q_OBJECT
 
- public:
-     VariantDelegate(QObject *parent = 0);
+  public:
+    VariantDelegate( QObject *parent = 0 );
 
-     void paint(QPainter *painter, const QStyleOptionViewItem &option,
-                const QModelIndex &index) const;
-     QWidget *createEditor(QWidget *parent, const QStyleOptionViewItem &option,
-                           const QModelIndex &index) const;
-     void setEditorData(QWidget *editor, const QModelIndex &index) const;
-     void setModelData(QWidget *editor, QAbstractItemModel *model,
-                       const QModelIndex &index) const;
+    void paint( QPainter *painter, const QStyleOptionViewItem &option,
+                const QModelIndex &index ) const;
+    QWidget *createEditor( QWidget *parent, const QStyleOptionViewItem &option,
+                           const QModelIndex &index ) const;
+    void setEditorData( QWidget *editor, const QModelIndex &index ) const;
+    void setModelData( QWidget *editor, QAbstractItemModel *model,
+                       const QModelIndex &index ) const;
 
-     static bool isSupportedType(QVariant::Type type);
-     static QString displayText(const QVariant &value);
+    static bool isSupportedType( QVariant::Type type );
+    static QString displayText( const QVariant &value );
 
-     static QVariant::Type type(const QVariant &value);
+    static QVariant::Type type( const QVariant &value );
 
- private:
-     mutable QRegExp boolExp;
-     mutable QRegExp byteArrayExp;
-     mutable QRegExp charExp;
-     mutable QRegExp colorExp;
-     mutable QRegExp dateExp;
-     mutable QRegExp dateTimeExp;
-     mutable QRegExp doubleExp;
-     mutable QRegExp pointExp;
-     mutable QRegExp rectExp;
-     mutable QRegExp signedIntegerExp;
-     mutable QRegExp sizeExp;
-     mutable QRegExp timeExp;
-     mutable QRegExp unsignedIntegerExp;
- };
+  private:
+    mutable QRegExp boolExp;
+    mutable QRegExp byteArrayExp;
+    mutable QRegExp charExp;
+    mutable QRegExp colorExp;
+    mutable QRegExp dateExp;
+    mutable QRegExp dateTimeExp;
+    mutable QRegExp doubleExp;
+    mutable QRegExp pointExp;
+    mutable QRegExp rectExp;
+    mutable QRegExp signedIntegerExp;
+    mutable QRegExp sizeExp;
+    mutable QRegExp timeExp;
+    mutable QRegExp unsignedIntegerExp;
+};
 
- #endif
+#endif
